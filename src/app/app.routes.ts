5f65c23cb1ab9464f4394c5524e4e296fc1b20de
Roumaysae <roumaysaeelamrani2002@gmail.com>
2024-05-13 08:34:38 +0100

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {AccountsComponent} from "./accounts/accounts.component";
+import {CustomersComponent} from "./customers/customers.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path : "customers", component:CustomersComponent},
+  {path : "accounts", component:AccountsComponent},
+];
